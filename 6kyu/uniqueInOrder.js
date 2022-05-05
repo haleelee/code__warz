6751d9775a0ed028c05359094bb82dd90a6379e7
haleelee <97466547+haleelee@users.noreply.github.com>
2022-05-05 08:50:00 -0500

    Updated file URL

--- 6kyu/uniqueInOrder.js
+++ 6kyu/uniqueInOrder.js
@@ -1,4 +1,4 @@
-// https://tv.youtube.com/watch/bqqeI0qMuHM?vpp=2AGZCg%3D%3D&vp=0gEEEgIwBA%3D%3D
+// https://www.codewars.com/kata/54e6533c92449cc251001667
 
 // Implement the function unique_in_order which takes as argument a sequence and returns a list of items without any elements with the same value next to each other and preserving the original order of elements.
 
