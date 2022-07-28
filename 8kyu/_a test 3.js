e94c9c6da0dadc9b905ff85087fe41fc1bc5869c
Michael Halili <97466547+haleelee@users.noreply.github.com>
2022-07-27 21:50:14 -0500

    Add files via upload

---
+++ 8kyu/_a test 3.js
@@ -0,0 +1,21 @@
+// https://www.codewars.com/kata/57eadb7ecd143f4c9c0000a3/train/javascript
+
+// Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+
+// The output should be two capital letters with a dot separating them.
+
+// It should look like this:
+
+// Sam Harris => S.H
+
+// patrick feeney => P.F
+
+function abbrevName(name){
+
+  // code away
+  let sepName = name.split(" ");
+  // console.log(`${sepName[0][0].toUpperCase()}.${sepName[1][0].toUpperCase()}`)
+  return `${sepName[0][0].toUpperCase()}.${sepName[1][0].toUpperCase()}`
+}
+
+abbrevName("patrick feeney")
