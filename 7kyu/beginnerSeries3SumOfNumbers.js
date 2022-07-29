ede01efd5f6185e2f13cdaa186df107430fc0d8a
Michael Halili <97466547+haleelee@users.noreply.github.com>
2022-07-29 08:34:44 -0500

    Add files via upload

---
+++ 7kyu/beginnerSeries3SumOfNumbers.js
@@ -0,0 +1,34 @@
+// https://www.codewars.com/kata/55f2b110f61eb01779000053/train/javascript
+
+// Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
+
+// Note: a and b are not ordered!
+
+// Examples (a, b) --> output (explanation)
+// (1, 0) --> 1 (1 + 0 = 1)
+// (1, 2) --> 3 (1 + 2 = 3)
+// (0, 1) --> 1 (0 + 1 = 1)
+// (1, 1) --> 1 (1 since both are same)
+// (-1, 0) --> -1 (-1 + 0 = -1)
+// (-1, 2) --> 2 (-1 + 0 + 1 + 2 = 2)
+
+function getSum( a,b )
+{
+  let sum = 0;
+
+  if(a < b){
+    for(a; a <= b; a++){
+      sum += a;
+    }
+  }else if(a > b){
+    for(b; b <= a; b++){
+      sum += b;
+    }
+  }else{
+    sum = a;
+  }
+  
+  return sum;
+}
+
+getSum(0,-3)//-6
